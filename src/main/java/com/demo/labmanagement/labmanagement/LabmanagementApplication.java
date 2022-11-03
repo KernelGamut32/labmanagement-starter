427dcf87f2402d1b95071ba645a638a860e1b52b
Allen Sanders <asanders@gamuttechnologysvcs.com>
2022-11-03 15:47:24 -0400

    initial check-in

---
+++ src/main/java/com/demo/labmanagement/labmanagement/LabmanagementApplication.java
@@ -0,0 +1,11 @@
+package com.demo.labmanagement.labmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabmanagementApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LabmanagementApplication.class, args);
+	}
+}
---
+++ src/main/tests/com/demo/labmanagement/labmanagement/LabmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arsazure.spring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
